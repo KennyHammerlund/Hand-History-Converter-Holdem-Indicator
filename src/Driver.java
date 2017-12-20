06702e89700a5b1beb7c8d189b58d40f652fa3cf
Kenny Hammerlund <kenny.hammerlund@gmail.com>
2017-12-20 16:58:54 -0600

    GUI Shell

---
+++ src/Driver.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+
+public class Driver {
+
+	public Driver() {
+	}
+
+	public static void main(String[] args) {
+		Application.launch(Display.class, args);
+	}
+}
